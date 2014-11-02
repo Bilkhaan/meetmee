1fc0327c0849f1e99c255bc806cf212ec931cfd3
bilkhan <bilal.khan@square63.com>
2014-11-02 10:40:29 +0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Meetmee::Application.config.secret_key_base = 'bf0f863a8d0b2a210bcc82f4c4b718af4c9ae656ed9f30330717303c22c27cdac061de3b8a05b4d8d1c63424a5b321218ba906570debb086ef5cc0ccff37d16f'
